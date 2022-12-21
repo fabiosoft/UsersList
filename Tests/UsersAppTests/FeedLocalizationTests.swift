59685703a408ec60d2f54fed0df062e23554fd23
Fabio Nisci <fabionisci@gmail.com>
2022-12-21 12:53:59 +0100

    localizations testing helpers should include only supported languages

--- Tests/UsersAppTests/FeedLocalizationTests.swift
+++ Tests/UsersAppTests/FeedLocalizationTests.swift
@@ -33,17 +33,19 @@ final class FeedLocalizationTests: XCTestCase {
 	private typealias LocalizedBundle = (bundle: Bundle, localization: String)
 
 	private func allLocalizationBundles(in bundle: Bundle, file: StaticString = #file, line: UInt = #line) -> [LocalizedBundle] {
-		return bundle.localizations.compactMap { localization in
-			guard
-				let path = bundle.path(forResource: localization, ofType: "lproj"),
-				let localizedBundle = Bundle(path: path)
-			else {
-				XCTFail("Couldn't find bundle for localization: \(localization)", file: file, line: line)
-				return nil
-			}
+		return bundle.localizations
+			.filter({ $0 != "Base" })
+			.compactMap { localization in
+				guard
+					let path = bundle.path(forResource: localization, ofType: "lproj"),
+					let localizedBundle = Bundle(path: path)
+				else {
+					XCTFail("Couldn't find bundle for localization: \(localization)", file: file, line: line)
+					return nil
+				}
 
-			return (localizedBundle, localization)
-		}
+				return (localizedBundle, localization)
+			}
 	}
 
 	private func allLocalizedStringKeys(in bundles: [LocalizedBundle], table: String, file: StaticString = #file, line: UInt = #line) -> Set<String> {
